515c46632215584e792dbd5109a1085a5ddac8c6
Joélson <joelsonjosue@hotmail.com>
2020-07-24 11:43:26 -0300

    feature/create-navbar

--- src/routes.js
+++ src/routes.js
@@ -2,13 +2,17 @@ import React from 'react'
 import {BrowserRouter, Switch, Route} from 'react-router-dom'
 import Home from './pages/Home'
 import TaskList from './pages/TaskList'
+import Navbar from './Navbar'
+import TaskForm from './pages/TaskForm'
 
 export default function Routes() {
     return (
         <BrowserRouter>
+        <Navbar />
             <Switch>
                 <Route exact path="/" component={Home} />
                 <Route exact path="/task-list" component={TaskList} />
+                <Route exact path="/task-form" component={TaskForm} />
             </Switch>
         </BrowserRouter>
     )
